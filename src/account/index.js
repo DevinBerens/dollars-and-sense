e6b9d340f0fc834498586f9fad83377a9d70c0e1
devinberens <devinlberens3@gmail.com>
2021-11-08 21:51:36 -0600

    built out the staff page

--- src/account/index.js
+++ src/account/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import CompanyLogo from 'companyLogo';
 
-let Account = props => {
+let Account = (props) => {
   return (
     <div className="indexWrapper">
       <CompanyLogo />
-      <div style={{ color: '#ffffff' }}>Account Index here</div>
+      <div style={{ color: '#ffffff' }}>
+        <div style={{ fontSize: '24px', display: 'flex' }}>
+          Applebees Account Page
+        </div>
+      </div>
     </div>
   );
 };
--- src/companyLogo.js
+++ src/companyLogo.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 let CompanyLogo = (props) => {
   return (
-    <div
+    <Link
       style={{
         position: 'absolute',
         top: '25px',
         left: '25px',
         fontSize: '28px',
         color: '#c5c6c7',
+        cursor: 'pointer',
+        textDecoration: 'none',
       }}
+      to="/portal/"
     >
       Dollars & Sense
-    </div>
+    </Link>
   );
 };
 export default CompanyLogo;
